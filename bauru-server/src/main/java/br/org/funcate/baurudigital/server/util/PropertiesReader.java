f3fb0954f099522e1b88fa04d2248ffc8e47be01
bogo <bogosoundz@gmail.com>
2013-11-14 18:35:11 -0200

    Jackson, Hibernate e Modelo de Dominio

--- bauru-server/src/main/java/br/org/funcate/baurudigital/server/util/PropertiesReader.java
+++ bauru-server/src/main/java/br/org/funcate/baurudigital/server/util/PropertiesReader.java
@@ -3,7 +3,7 @@ package br.org.funcate.baurudigital.server.util;
 import java.io.IOException;
 import java.util.Properties;
 
-import br.org.funcate.baurudigital.server.model.exception.ConfigException;
+import br.org.funcate.baurudigital.user.controller.exception.ConfigException;
 
 
 
@@ -20,26 +20,5 @@ public class PropertiesReader {
 			}
 			return p.getProperty(name);
 		}
-		/*
-try {
-        uri = url.toURI();
-} catch (URISyntaxException e1) {
-        e1.printStackTrace();
-        JOptionPane.showMessageDialog(null,
-                        "Falha ao carregar o arquivo .properties");
-}
-
-file = new File(uri);
-fis = null;
-
-try {
-        fis = new FileInputStream(file);
-        properties.load(fis);
-        fis.close();
-} catch (IOException e) {
-        System.err.println(e.getMessage());
-        JOptionPane.showMessageDialog(null,
-                        "Falha ao ler o arquivo .properties");
-}*/
 
 }
