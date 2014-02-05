9217d7f2a57b162177df0b2892aaf08ffafdf53b
Paulo Luan <paulonfsu2@hotmail.com>
2014-02-05 09:56:46 -0200

    Fixed path of the base map.

--- app/src/br/inpe/mobile/task/DownloadZipAsync.java
+++ app/src/br/inpe/mobile/task/DownloadZipAsync.java
@@ -7,6 +7,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.zip.ZipEntry;
@@ -21,172 +23,173 @@ import br.inpe.mobile.exception.ExceptionHandler;
 import br.inpe.mobile.map.GeoMap;
 
 public class DownloadZipAsync extends AsyncTask<String, String, String> {
-    String               tileSourcePath = GeoMap.tileSourcePath;
-    String               filePath = null;
-    
-    private TaskActivity taskActivity;
-    
-    public DownloadZipAsync(TaskActivity taskActivity) {
-        this.taskActivity = taskActivity;
-    }
-    
-    @Override
-    protected String doInBackground(String... urls) {
-        String message = null;
-        
-        try {
-            String zipFilePath = Environment.getExternalStorageDirectory() + "/inpe/dados";
-            filePath = zipFilePath + "base_map.zip";
-            File baseMapZip = new File(filePath);
-            
-            if (!baseMapZip.exists()) {
-                this.getRemoteBaseMap(urls[0]);
-            }
-            else {
-                long localFileSize = baseMapZip.length();
-                boolean isSameSize = this.compareSizeOfRemoteFile(urls[0], localFileSize);
-                
-                if (!isSameSize) {
-                    this.getRemoteBaseMap(urls[0]);
-                }
-            }
-        }
-        catch (Exception e) {
-            message = "Ocorreu um erro ao baixar o arquivo.";
-            ExceptionHandler.saveLogFile(e.toString());
-        }
-        
-        try {
-            this.unzip(filePath, tileSourcePath);
-        }
-        catch (Exception e) {
-            message = "Ocorreu um erro ao descompactar o arquivo.";
-            ExceptionHandler.saveLogFile(e.toString());
-        }
-        
-        return message;
-    }
-    
-    public boolean compareSizeOfRemoteFile(String remoteUrl, long localFileSize) throws IOException {
-        boolean isSameSize = false;
-        
-        URL url = new URL(remoteUrl);
-        URLConnection conexion = url.openConnection();
-        conexion.connect();
-        
-        long remoteFileSize = conexion.getContentLength();
-        
-        if (localFileSize == remoteFileSize) {
-            isSameSize = true;
-        }
-        
-        return isSameSize;
-    }
-    
-    /**
-     * Get remote file using URLConnection.
-     * 
-     * @param remoteURL
-     *            The remote url of the file.
-     * 
-     * @throws IOException
-     */
-    public void getRemoteBaseMap(String remoteUrl) throws IOException {
-        int count;
-        
-        URL url = new URL(remoteUrl);
-        URLConnection conexion = url.openConnection();
-        conexion.connect();
-        
-        int fileSize = conexion.getContentLength();
-        
-        Utility.dirChecker(filePath); //TODO: testar
-        
-        InputStream input = new BufferedInputStream(url.openStream());
-        OutputStream output = new FileOutputStream(filePath);
-        
-        byte data[] = new byte[1024];
-        long total = 0;
-        
-        while ((count = input.read(data)) != -1) {
-            total += count;
-            Integer progress = (int) ((total * 100) / fileSize);
-            publishProgress("Baixando mapa de base...", "" + progress);
-            output.write(data, 0, count);
-        }
-        
-        output.flush();
-        output.close();
-        input.close();
-    }
-    
-    /**
-     * Unzip files.
-     * 
-     * @param zipfile
-     *            The path of the zip file
-     * @param location
-     *            The new Location (path) that you want to unzip the file
-     * @throws IOException
-     */
-    private void unzip(String zipFile, String outputFolder) throws IOException {
-        int progress = 0;
-        
-        ZipFile zip = new ZipFile(zipFile);
-        publishProgress("Descompactando mapa de base...", "0", "" + zip.size());
-        
-        FileInputStream fin = new FileInputStream(zipFile);
-        ZipInputStream zin = new ZipInputStream(fin);
-        ZipEntry ze = null;
-        
-        while ((ze = zin.getNextEntry()) != null) {
-            if (ze.isDirectory()) {
-                Utility.dirChecker(outputFolder + ze.getName());
-            }
-            else {
-                progress++;
-                publishProgress("Descompactando mapa de base...", "" + progress);
-                
-                FileOutputStream fout = null;
-                try {
-                    fout = new FileOutputStream(outputFolder + ze.getName());
-                    for (int c = zin.read(); c != -1; c = zin.read()) {
-                        fout.write(c);
-                    }
-                }
-                catch (Exception e) {
-                    ExceptionHandler.saveLogFile(e.toString());
-                }
-                finally {
-                    zin.closeEntry();
-                    if (fout != null) {
-                        fout.close();
-                    }
-                }
-            }
-        }
-        
-        zin.close();
-    }
-    
-    @Override
-    protected void onPreExecute() {
-        super.onPreExecute();
-        taskActivity.showLoadingMask("Carregando, aguarde...");
-    }
-    
-    @Override
-    protected void onProgressUpdate(String... progress) {
-        super.onProgressUpdate(progress);
-        taskActivity.onProgressUpdate(progress);
-    }
-    
-    @Override
-    protected void onPostExecute(String message) {
-        taskActivity.hideLoadingMask();
-        
-        if (message != null) {
-            Utility.showToast(message, Toast.LENGTH_LONG, taskActivity);
-        }
-    }
+	String tileSourcePath = GeoMap.tileSourcePath;
+	String filePath = null;
+
+	private TaskActivity taskActivity;
+
+	public DownloadZipAsync(TaskActivity taskActivity) {
+		this.taskActivity = taskActivity;
+	}
+
+	@Override
+	protected String doInBackground(String... urls) {
+		String message = null;
+
+		try {
+			String zipFilePath = Environment.getExternalStorageDirectory()
+					+ "/inpe/dados/";
+			filePath = zipFilePath + "base_map.zip";
+			File baseMapZip = new File(filePath);
+
+			if (!baseMapZip.exists()) {
+				this.getRemoteBaseMap(urls[0]);
+			} else {
+				long localFileSize = baseMapZip.length();
+				boolean isSameSize = this.compareSizeOfRemoteFile(urls[0],
+						localFileSize);
+
+				if (!isSameSize) {
+					this.getRemoteBaseMap(urls[0]);
+				}
+			}
+		} catch (Exception e) {
+			message = "Ocorreu um erro ao baixar o arquivo.";
+			StringWriter errors = new StringWriter();
+			e.printStackTrace(new PrintWriter(errors));
+			ExceptionHandler.saveLogFile(errors.toString());
+		}
+
+		try {
+			this.unzip(filePath, tileSourcePath);
+		} catch (Exception e) {
+			message = "Ocorreu um erro ao descompactar o arquivo.";
+			StringWriter errors = new StringWriter();
+			e.printStackTrace(new PrintWriter(errors));
+			ExceptionHandler.saveLogFile(errors.toString());
+		}
+
+		return message;
+	}
+
+	public boolean compareSizeOfRemoteFile(String remoteUrl, long localFileSize)
+			throws IOException {
+		boolean isSameSize = false;
+
+		URL url = new URL(remoteUrl);
+		URLConnection conexion = url.openConnection();
+		conexion.connect();
+
+		long remoteFileSize = conexion.getContentLength();
+
+		if (localFileSize == remoteFileSize) {
+			isSameSize = true;
+		}
+
+		return isSameSize;
+	}
+
+	/**
+	 * Get remote file using URLConnection.
+	 * 
+	 * @param remoteURL
+	 *            The remote url of the file.
+	 * 
+	 * @throws IOException
+	 */
+	public void getRemoteBaseMap(String remoteUrl) throws IOException {
+		int count;
+
+		URL url = new URL(remoteUrl);
+		URLConnection conexion = url.openConnection();
+		conexion.connect();
+
+		int fileSize = conexion.getContentLength();
+
+		InputStream input = new BufferedInputStream(url.openStream());
+		OutputStream output = new FileOutputStream(filePath);
+
+		byte data[] = new byte[1024];
+		long total = 0;
+
+		while ((count = input.read(data)) != -1) {
+			total += count;
+			Integer progress = (int) ((total * 100) / fileSize);
+			publishProgress("Baixando mapa de base...", "" + progress);
+			output.write(data, 0, count);
+		}
+
+		output.flush();
+		output.close();
+		input.close();
+	}
+
+	/**
+	 * Unzip files.
+	 * 
+	 * @param zipfile
+	 *            The path of the zip file
+	 * @param location
+	 *            The new Location (path) that you want to unzip the file
+	 * @throws IOException
+	 */
+	private void unzip(String zipFile, String outputFolder) throws IOException {
+		int progress = 0;
+
+		ZipFile zip = new ZipFile(zipFile);
+		publishProgress("Descompactando mapa de base...", "0", "" + zip.size());
+
+		FileInputStream fin = new FileInputStream(zipFile);
+		ZipInputStream zin = new ZipInputStream(fin);
+		ZipEntry ze = null;
+
+		while ((ze = zin.getNextEntry()) != null) {
+			if (ze.isDirectory()) {
+				Utility.dirChecker(outputFolder + ze.getName());
+			} else {
+				progress++;
+				publishProgress("Descompactando mapa de base...", "" + progress);
+
+				FileOutputStream fout = null;
+				try {
+					fout = new FileOutputStream(outputFolder + ze.getName());
+					for (int c = zin.read(); c != -1; c = zin.read()) {
+						fout.write(c);
+					}
+				} catch (Exception e) {
+					StringWriter errors = new StringWriter();
+					e.printStackTrace(new PrintWriter(errors));
+					ExceptionHandler.saveLogFile(errors.toString());
+				} finally {
+					zin.closeEntry();
+					if (fout != null) {
+						fout.close();
+					}
+				}
+			}
+		}
+
+		zin.close();
+	}
+
+	@Override
+	protected void onPreExecute() {
+		super.onPreExecute();
+		taskActivity.showLoadingMask("Carregando, aguarde...");
+	}
+
+	@Override
+	protected void onProgressUpdate(String... progress) {
+		super.onProgressUpdate(progress);
+		taskActivity.onProgressUpdate(progress);
+	}
+
+	@Override
+	protected void onPostExecute(String message) {
+		taskActivity.hideLoadingMask();
+
+		if (message != null) {
+			Utility.showToast(message, Toast.LENGTH_LONG, taskActivity);
+		}
+	}
 }
