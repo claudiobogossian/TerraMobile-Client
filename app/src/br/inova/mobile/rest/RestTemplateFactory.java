8f2a3e96b8633e94bd220afeb5a0f9673eb192d7
Paulo Luan <pauloluan.inova@gmail.com>
2014-06-10 16:03:18 -0300

    Added more time to timeout.

--- app/src/br/inova/mobile/rest/RestTemplateFactory.java
+++ app/src/br/inova/mobile/rest/RestTemplateFactory.java
@@ -13,12 +13,12 @@ public class RestTemplateFactory extends RestTemplate {
         public RestTemplateFactory() {
                 if (getRequestFactory() instanceof SimpleClientHttpRequestFactory) {
                         Log.d("HTTP", "HttpUrlConnection is used");
-                        ((SimpleClientHttpRequestFactory) getRequestFactory()).setConnectTimeout(1000);
+                        ((SimpleClientHttpRequestFactory) getRequestFactory()).setConnectTimeout(25 * 1000);
                         ((SimpleClientHttpRequestFactory) getRequestFactory()).setReadTimeout(60 * 1000);
                 }
                 else if (getRequestFactory() instanceof HttpComponentsClientHttpRequestFactory) {
                         Log.d("HTTP", "HttpClient is used");
-                        ((HttpComponentsClientHttpRequestFactory) getRequestFactory()).setConnectTimeout(1000);
+                        ((HttpComponentsClientHttpRequestFactory) getRequestFactory()).setConnectTimeout(25 * 1000);
                         ((HttpComponentsClientHttpRequestFactory) getRequestFactory()).setReadTimeout(60 * 1000);
                 }
                 
